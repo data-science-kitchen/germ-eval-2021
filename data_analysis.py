b3cc3d17aa812f54b6852452be8da25677ea14a4
Christopher <ch.schymura@gmail.com>
2021-06-20 18:38:08 +0200

    Finish data analysis framework.

--- data_analysis.py
+++ data_analysis.py
@@ -2,9 +2,25 @@ import matplotlib.pyplot as plt
 from features import *
 import pandas as pd
 import seaborn as sns
+from tqdm import tqdm
 
 
 dataset = pd.read_csv('./data/GermEval21_Toxic_Train.csv')
+
+sentiment_model = SentimentModel()
+
+
+def positive_sentiment(x: str) -> float:
+    return positive_sentiment_logits(x, sentiment_model)
+
+
+def negative_sentiment(x: str) -> float:
+    return negative_sentiment_logits(x, sentiment_model)
+
+
+def neutral_sentiment(x: str) -> float:
+    return neutral_sentiment_logits(x, sentiment_model)
+
 features = [
     log_num_characters, log_average_word_length, log_word_length_std, positive_sentiment, negative_sentiment,
     neutral_sentiment
@@ -13,7 +29,7 @@ features = [
 for feature in features:
     dataset.insert(0, feature.__name__, np.nan)
 
-for row in dataset.iterrows():
+for row in tqdm(dataset.iterrows(), total=len(dataset)):
     idx, data = row
 
     for feature in features:
--- features.py
+++ features.py
@@ -9,7 +9,7 @@ def log_num_characters(text: str) -> float:
     Args:
         text (str):
     """
-    return np.log(len(text))
+    return np.log(len(text) + 1e-9)
 
 
 def log_num_tokens(text: str) -> float:
@@ -17,7 +17,7 @@ def log_num_tokens(text: str) -> float:
     Args:
         text (str):
     """
-    return np.log(len(text.split()))
+    return np.log(len(text.split()) + 1e-9)
 
 
 def log_average_word_length(text: str) -> float:
@@ -27,7 +27,7 @@ def log_average_word_length(text: str) -> float:
     """
     word_lengths = [len(x) for x in text.split()]
 
-    return np.log(np.mean(word_lengths))
+    return np.log(np.mean(word_lengths) + 1e-9)
 
 
 def log_word_length_std(text: str) -> float:
@@ -37,40 +37,66 @@ def log_word_length_std(text: str) -> float:
     """
     word_lengths = [len(x) for x in text.split()]
 
-    return np.log(np.std(word_lengths))
+    return np.log(np.std(word_lengths) + 1e-9)
 
 
-def positive_sentiment(text: str) -> float:
-    """
-    Args:
-        text (str):
-    """
-    text = _sentiment_clean_text(text)
-    logits = _sentiment_get_logits(text, max_num_tokens=512)
-
-    return logits[0]
+def positive_sentiment_logits(text: str, model: callable) -> float:
+    return model(text)[0]
 
 
-def negative_sentiment(text: str) -> float:
-    """
-    Args:
-        text (str):
-    """
-    text = _sentiment_clean_text(text)
-    logits = _sentiment_get_logits(text, max_num_tokens=512)
-
-    return logits[1]
+def negative_sentiment_logits(text: str, model: callable) -> float:
+    return model(text)[1]
 
 
-def neutral_sentiment(text: str) -> float:
-    """
-    Args:
-        text (str):
-    """
-    text = _sentiment_clean_text(text)
-    logits = _sentiment_get_logits(text, max_num_tokens=512)
+def neutral_sentiment_logits(text: str, model: callable) -> float:
+    return model(text)[2]
 
-    return logits[2]
+
+class SentimentModel:
+    MAX_NUM_TOKENS = 512
+
+    def __init__(self) -> None:
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        self.model = AutoModelForSequenceClassification.from_pretrained('oliverguhr/german-sentiment-bert')
+        self.model = self.model.to(self.device)
+
+        self.tokenizer = AutoTokenizer.from_pretrained('oliverguhr/german-sentiment-bert')
+        self.clean_chars = re.compile(r'[^A-Za-züöäÖÜÄß ]', re.MULTILINE)
+        self.clean_http_urls = re.compile(r'https*\S+', re.MULTILINE)
+        self.clean_at_mentions = re.compile(r'@\S+', re.MULTILINE)
+
+    def __call__(self, text: str) -> float:
+        text = self._clean_text(text)
+
+        input_ids = self.tokenizer.batch_encode_plus([text], padding=True, add_special_tokens=True)
+        input_ids = torch.tensor(input_ids['input_ids'])
+        input_ids = input_ids.to(self.device)
+
+        if input_ids.shape[-1] > self.MAX_NUM_TOKENS:
+            input_ids = input_ids[:, :self.MAX_NUM_TOKENS]
+
+        with torch.no_grad():
+            result = self.model(input_ids)
+
+        return result.logits.squeeze().detach().cpu().numpy()
+
+    def _clean_text(self, text: str) -> str:
+        text = text.replace("\n", " ")
+        text = self.clean_http_urls.sub('', text)
+        text = self.clean_at_mentions.sub('', text)
+        text = self._replace_numbers(text)
+        text = self.clean_chars.sub('', text)
+        text = ' '.join(text.split())
+        text = text.strip().lower()
+
+        return text
+
+    @staticmethod
+    def _replace_numbers(text: str) -> str:
+        return text.replace('0', ' null').replace('1', ' eins').replace('2', ' zwei').replace('3', ' drei') \
+            .replace('4', ' vier').replace('5', ' fünf').replace('6', ' sechs').replace('7', ' sieben') \
+            .replace('8', ' acht').replace('9', ' neun')
 
 
 def _sentiment_clean_text(text: str) -> str:
